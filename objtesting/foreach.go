9869456a2aacb9e444488d22fbd4392a5d56c34b
matthewhudsonedb <matthew.hudson@enterprisedb.com>
2026-01-16 13:17:27 +1100

    update tests to allow for testing azure and  datalake storage accounts
    Signed-off-by: matthewhudsonedb <matthew.hudson@enterprisedb.com>

--- objtesting/foreach.go
+++ objtesting/foreach.go
@@ -101,10 +101,8 @@ func ForeachStore(t *testing.T, testFn func(t *testing.T, bkt objstore.Bucket)) 
 		})
 	}
 
-	isAzureDataLakeGen2 := (os.Getenv("IS_AZURE_DATA_LAKE_GEN2") == "true")
-
-	// Optional Azure.
-	if !IsObjStoreSkipped(t, objstore.AZURE) && !isAzureDataLakeGen2 {
+	// Optional Azure (automatically detects Blob vs Data Lake Gen2).
+	if !IsObjStoreSkipped(t, objstore.AZURE) {
 		t.Run("azure", func(t *testing.T) {
 			bkt, closeFn, err := azure.NewTestBucket(t, "e2e-tests")
 			testutil.Ok(t, err)
@@ -115,20 +113,23 @@ func ForeachStore(t *testing.T, testFn func(t *testing.T, bkt objstore.Bucket)) 
 			testFn(t, bkt)
 			testFn(t, objstore.NewPrefixedBucket(bkt, "some_prefix"))
 		})
-	}
 
-	// Optional Azure Data Lake Gen 2
-	if !IsObjStoreSkipped(t, objstore.AZURE) && isAzureDataLakeGen2 {
-		t.Run("azure", func(t *testing.T) {
-			bkt, closeFn, err := azure.NewTestDataLakeGen2Bucket(t, "e2e-tests")
-			testutil.Ok(t, err)
+		// If separate Data Lake Gen2 credentials are provided, test autodiscovery with both.
+		if datalakeAccount, datalakeKey := os.Getenv("AZURE_STORAGE_ACCOUNT_DATALAKE"), os.Getenv("AZURE_STORAGE_ACCESS_KEY_DATALAKE"); datalakeAccount != "" && datalakeKey != "" {
+			t.Run("azure data lake gen2", func(t *testing.T) {
+				os.Setenv("AZURE_STORAGE_ACCOUNT", datalakeAccount)
+				os.Setenv("AZURE_STORAGE_ACCESS_KEY", datalakeKey)
 
-			t.Parallel()
-			defer closeFn()
+				bkt, closeFn, err := azure.NewTestBucket(t, "e2e-tests")
+				testutil.Ok(t, err)
 
-			testFn(t, bkt)
-			testFn(t, objstore.NewPrefixedBucket(bkt, "some_prefix"))
-		})
+				t.Parallel()
+				defer closeFn()
+
+				testFn(t, bkt)
+				testFn(t, objstore.NewPrefixedBucket(bkt, "some_prefix"))
+			})
+		}
 	}
 
 	// Optional SWIFT.
